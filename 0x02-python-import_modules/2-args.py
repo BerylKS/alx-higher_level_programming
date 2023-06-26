b1db7d6b57b79b666761e86e350c47a53c34ffd1
Beryl KS <berylks22@gmail.com>
2023-06-26 00:14:59 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+n = len(sys.argv) - 1
+if n == 0:
+    print("0 arguments.")
+elif n == 1:
+    print("1 argument:")
+else:
+    print(f"{n} arguments:")
+for i in range(n):
+    print(f"{i + 1}: {sys.argv[i + 1]}")
