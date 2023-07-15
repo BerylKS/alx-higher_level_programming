57348195b0a13ce3b60ca267fe836b17e2af664f
Beryl KS <berylks22@gmail.com>
2023-07-15 12:30:53 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if i == 8 and j == 9:
-            print(f"{i}{j}")
+            print("{}{}".format(i, j))
         elif j > i:
-            print(f"{i}{j}", end=", ")
+            print("{}{}".format(i, j), end=", ")
         else:
             pass
