98056f51e8b03621a2296dfb831dfd233d5325c8
Beryl KS <berylks22@gmail.com>
2023-07-16 01:18:03 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import sys
-
-add = 0
-n = len(sys.argv) - 1
-for i in range(n):
-    add = add + int(sys.argv[i + 1])
-print(f"{add}")
+if __name__ == "__main__":
+    import sys
+    add = 0
+    n = len(sys.argv) - 1
+    for i in range(n):
+        add = add + int(sys.argv[i + 1])
+    print("{}".format(add))
